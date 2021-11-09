a43c1d6d5ccc85a10d87fda52d26214ba012859f
Eralp <eralpozcans@gmail.com>
2021-11-09 20:26:23 +0300

    Small fix home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
+  <Navbar />
 </template>
 
 <script>
-
+import Navbar from '../components/Navbar.vue';
 
 export default {
   name: "Home",
 
   components: {
+    Navbar
   },
 };
 </script>
